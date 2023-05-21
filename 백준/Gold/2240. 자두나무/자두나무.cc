28bc5b64d8c401e23040407927b3f0f520067a49
obdo <81908835+fortress43-dev@users.noreply.github.com>
2023-05-22 01:17:44 +0900

    [Gold V] Title: 자두나무, Time: 0 ms, Memory: 2280 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.cc
+++ 백준/Gold/2240. 자두나무/자두나무.cc
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+//자두나무
 
 void solve() {
     int t; cin >> t;
     int w; cin >> w;
 
-    int dp[3][1001][32];
-    int arr[1001];
+    int dp[3][1001][32];//자두나무 위치, 자두가 떨어지는 초, 움직이는 횟수
+    int arr[1001];      //자두 떨어지는 나무 정보
     int ans = 0;
 
     for (int i = 1; i <= t; i++) {
@@ -15,24 +16,20 @@ void solve() {
     }
 
     for (int i = 1; i <= t; i++) {
+        int appleTree = arr[i];
         for (int j = 1; j <= w + 1; j++) {
-            if (arr[i] == 1) {
-                dp[1][i][j] = max(dp[1][i - 1][j] + 1, dp[2][i - 1][j - 1] + 1);
-                dp[2][i][j] = max(dp[1][i - 1][j - 1], dp[2][i - 1][j]);
-            }
-            else {
-                if (i == 1 && j == 1) {
-                    continue;
-                }
-                dp[1][i][j] = max(dp[2][i - 1][j - 1], dp[1][i - 1][j]);
-                dp[2][i][j] = max(dp[1][i - 1][j - 1] + 1, dp[2][i - 1][j] + 1);
-            }
+            if (appleTree == 2 && i == 1 && j == 1)continue;
+
+            dp[1][i][j] = max(dp[1][i - 1][j], dp[2][i - 1][j - 1]);     
+            dp[2][i][j] = max(dp[1][i - 1][j - 1], dp[2][i - 1][j]);
+            
+            dp[appleTree][i][j]++;
         }
     }
     for (int i = 1; i <= w + 1; i++) {
         ans = max(ans, max(dp[1][t][i], dp[2][t][i]));
     }
-    cout<<ans;
+    cout << ans;
 }
 
 int main() {

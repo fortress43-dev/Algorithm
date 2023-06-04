e2beb2830e072dee9b948e172b8e63dc297389b9
obdo <81908835+fortress43-dev@users.noreply.github.com>
2023-06-04 21:57:20 +0900

    [Silver V] Title: 단어 정렬, Time: 80 ms, Memory: 4108 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int cmp(string a, string b) {
+	if (a.length() == b.length())return a < b; //길이가 같다면 사전순
+	else return a.length() < b.length();		//다른다면 짧은 순
+}
+
+string word[20000];
+
+int main() {
+	int N;
+
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> word[i];
+	}
+	sort(word, word + N, cmp);
+
+	for (int i = 0; i < N; i++)
+	{
+		if (word[i] == word[i - 1]) {
+			continue;
+		}
+		cout << word[i] << "\n";
+	}
+}
